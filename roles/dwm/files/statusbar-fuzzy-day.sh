24760bcb8cf9f6a93ee06195bbea7723c282bbca
Johan Svan <johan@svan.dev>
2020-03-03 15:11:30 +0100

    Corrected some dates
    Some dates where printing incorrectly

--- roles/dwm/files/statusbar-fuzzy-day.sh
+++ roles/dwm/files/statusbar-fuzzy-day.sh
@@ -10,12 +10,15 @@ __fuzzy_day_dom(){
   local -r n="$(LC_ALL=C date +'%d')"
 
   case "$n" in
-    01) echo "${n}st";;
-    02) echo "${n}nd";;
-    03) echo "${n}d";;
-    [0-1]*) echo "${n}th";;
+    01) echo "${n:1}st";;
+    02) echo "${n:1}nd";;
+    03) echo "${n:1}d";;
+    0*) echo "${n:1}th";;
+    1*) echo "${n}th";;
     [2-3]0) echo "${n}th";;
-    [1-3]1) echo "${n}st";;
+    [2-3]1) echo "${n}st";;
+    [2-3]2) echo "${n}nd";;
+    2*) echo "${n}th";;
   esac
 }
 
